0a5c7543316dee918ce1d05e7f817f0f3539b774
Shanti Bouchez-Mongardé <shanti-pub.git@mildred.fr>
2014-07-11 12:51:26 +0200

    update.sh

--- update.sh
+++ update.sh
@@ -8,7 +8,11 @@ pull(){
   ( cd "$root"
     git pull --ff-only
   )
-  exec "$zero" the-rest
+  if "$zero" the-rest; then
+    echo "Success"
+  else
+    echo "Failure"
+  fi
 }
 
 case "$1" in
@@ -16,11 +20,8 @@ case "$1" in
     cd "$root"
     "$PWD/sysvservice.sh" install-deps
     "$PWD/sysvservice.sh" install-init.d
-    "$PWD/sysvservice.sh" enable
+    "$PWD/sysvservice.sh" enable || echo "Error ignored"
     "$PWD/sysvservice.sh" restart
-
-    echo
-    echo "Success"
     exit 0
   ;;
   *)
