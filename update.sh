5bb34e0fe07217aca050876aef85ddc2fe3c6e29
Shanti Bouchez-Mongardé <shanti-pub.git@mildred.fr>
2014-07-11 12:52:41 +0200

    update.sh: npm install

--- update.sh
+++ update.sh
@@ -18,6 +18,7 @@ pull(){
 case "$1" in
   the-rest)
     cd "$root"
+    npm install
     "$PWD/sysvservice.sh" install-deps
     "$PWD/sysvservice.sh" install-init.d
     "$PWD/sysvservice.sh" enable || echo "Error ignored"
